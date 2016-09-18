c43c07276660780dcd39c41ab82fdb8d94c88aa4
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-09-18 17:21:42 +0100

    Tests passed and written up code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,29 @@
 class FizzBuzz
-	def divisible_by_3(number)
+	def divisible_by_three(number)
 		if number % 3 == 0
 			"Fizz"
 		else
 			number
 		end
+	end
+
+	def divisible_by_five(number)
+		if number % 5 == 0
+			"Buzz"
+		else
+			number
+		end
+	end
+
+	def divisible_by_fifteen(number)
+		if number % 15 == 0
+			"FizzBuzz"
+		else
+			number
+		end
+	end
+
+	def divisible_by_any_number(number)
+		number
 	end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,29 @@ require 'FizzBuzz'
 require 'spec_helper'
 
 describe FizzBuzz do
-  it 'if a number is divisible by 3' do
-  	expect(divisible_by_3).to eq("Fizz")
+  context 'if a number is divisible by' do
+  	it '3 print Fizz' do
+      fizz_buzz = FizzBuzz.new
+  	  result = fizz_buzz.divisible_by_three(3)
+  	  expect(result).to eq("Fizz")
+    end
+
+    it '5 print Buzz' do
+      fizz_buzz = FizzBuzz.new
+  	  result = fizz_buzz.divisible_by_five(5)
+  	  expect(result).to eq("Buzz")
+    end
+
+    it '15 print FizzBuzz' do
+      fizz_buzz = FizzBuzz.new
+      result = fizz_buzz.divisible_by_fifteen(15)
+      expect(result).to eq("FizzBuzz")
+    end 
+
+    it 'any other numbers' do
+    	fizz_buzz = FizzBuzz.new
+    	result = fizz_buzz.divisible_by_any_number(1)
+    	expect(result).to eq(1)
+    end
   end
 end
