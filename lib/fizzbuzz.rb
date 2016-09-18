b06bc87ebfa050ebb7b874a9cc94f860c24199f1
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-09-18 23:40:34 +0100

    Refactored tests and code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,29 +1,45 @@
+# class FizzBuzz
+# 	def divisible_by_three(number)
+# 		if number % 3 == 0
+# 			"Fizz"
+# 		else
+# 			number
+# 		end
+# 	end
+
+# 	def divisible_by_five(number)
+# 		if number % 5 == 0
+# 			"Buzz"
+# 		else
+# 			number
+# 		end
+# 	end
+
+# 	def divisible_by_fifteen(number)
+# 		if number % 15 == 0
+# 			"FizzBuzz"
+# 		else
+# 			number
+# 		end
+# 	end
+
+# 	def divisible_by_any_number(number)
+# 		number
+# 	end
+# end
+
 class FizzBuzz
-	def divisible_by_three(number)
-		if number % 3 == 0
-			"Fizz"
-		else
-			number
-		end
-	end
-
-	def divisible_by_five(number)
-		if number % 5 == 0
-			"Buzz"
-		else
-			number
-		end
-	end
-
-	def divisible_by_fifteen(number)
-		if number % 15 == 0
-			"FizzBuzz"
-		else
-			number
-		end
-	end
-
-	def divisible_by_any_number(number)
-		number
-	end
+  def divisible_by(number)
+    if number % 15 == 0
+      "FizzBuzz"
+    elsif number % 3 == 0
+      "Fizz"
+    elsif number % 5 == 0
+      "Buzz"
+    else
+      number
+  end
 end
+end
+	
+    	
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,28 +2,26 @@ require 'FizzBuzz'
 require 'spec_helper'
 
 describe FizzBuzz do
-  context 'if a number is divisible by' do
+  context 'when a number is divisible by' do
+  	let(:fizz_buzz) { FizzBuzz.new }
+
   	it '3 print Fizz' do
-      fizz_buzz = FizzBuzz.new
-  	  result = fizz_buzz.divisible_by_three(3)
+  	  result = fizz_buzz.divisible_by(3)
   	  expect(result).to eq("Fizz")
     end
 
     it '5 print Buzz' do
-      fizz_buzz = FizzBuzz.new
-  	  result = fizz_buzz.divisible_by_five(5)
+  	  result = fizz_buzz.divisible_by(5)
   	  expect(result).to eq("Buzz")
     end
 
     it '15 print FizzBuzz' do
-      fizz_buzz = FizzBuzz.new
-      result = fizz_buzz.divisible_by_fifteen(15)
+      result = fizz_buzz.divisible_by(15)
       expect(result).to eq("FizzBuzz")
     end 
 
-    it 'any other numbers' do
-    	fizz_buzz = FizzBuzz.new
-    	result = fizz_buzz.divisible_by_any_number(1)
+    it 'any other number' do
+    	result = fizz_buzz.divisible_by(1)
     	expect(result).to eq(1)
     end
   end
